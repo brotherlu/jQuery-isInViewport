8c59b488b7f4129ce4b73e9630a7a0e1d044411a
Louay Cheikh <brotherlu@gmail.com>
2015-02-25 00:37:33 -0500

    Added support for unique focus elements Added Comments

--- src/js/isInViewport.js
+++ src/js/isInViewport.js
@@ -1,44 +1,70 @@
-(function($){
+(function($, window){
 
   function isInViewport(elem){
     var elemTop = $(elem).offset().top,
         elemBottom = elemTop + $(elem).height(),
         viewportTop = $(window).scrollTop(),
-        viewportBottom = viewportTop + $(window).height();
+        viewportBottom = viewportTop + $(window).height(),
+        topDistance = ((viewportBottom - elemTop) >= 0) ? viewportBottom - elemTop : 0,
+        bottomDistance = ((elemBottom - viewportTop) >= 0) ? elemBottom - viewportTop : 0,
+        percentageOfViewport = (Math.min($(elem).height(),topDistance, bottomDistance ))/$(window).height(),
+        percentageOfElement = (Math.min($(elem).height(),topDistance, bottomDistance ))/$(elem).height();
 
-   /* console.table ({
-      element: this.className,
-      elemTop: elemTop,
-      elemBottom: elemBottom,
-      viewportTop: viewportTop,
-      viewportBottom: viewportBottom,
-      isVisible: elemTop <= viewportBottom && elemBottom >= viewportTop
-    });*/
-
-    return (elemTop < viewportBottom && elemBottom >= viewportTop);
+    return {
+      isVisible: (elemTop < viewportBottom && elemBottom >= viewportTop),
+      percentageOfViewport: percentageOfViewport,
+      percentageOfElement: percentageOfElement
+    };
   }
 
   $.fn.isInViewport = function(options,callback,exitCallback){
 
+    // prevent any errors by defining a default value
+    // not classy but does the job
     callback = callback || function(el){};
     exitCallback = exitCallback || function(el){};
 
+    // default settings
     var settings = {
-      className: 'isVisible'
+      className: 'isVisible',
+      focusName: 'isFocus'
     };
 
+    // Extend the settings with the inputted options
     $.extend(settings, options);
+    
+    //
+    var maxPercent = 0;
 
     return this.each(function(){
-      if(isInViewport(this) && !$(this).hasClass(settings.className)){
+      // Check for visible elements
+      if(isInViewport(this).isVisible){
+        
+        // Add ClassName
         $(this).addClass(settings.className);
+
+        // Check if the element is the most visible element
+        if(isInViewport(this).percentageOfViewport > maxPercent){
+          
+          // Store the new max view percentage
+          maxPercent = isInViewport(this).percentageOfViewport;
+          // Clear old focus element
+          $('.'+settings.focusName).removeClass(settings.focusName);
+          // Add New focus element class
+          $(this).addClass(settings.focusName);
+        }
+        // Execute the callback
         callback(this);
-      } else if (!isInViewport(this) && $(this).hasClass(settings.className)){
-        $(this).removeClass(settings.className);
+
+        // Cleanup all non visible elements
+      } else if (!isInViewport(this).isVisible && $(this).hasClass(settings.className)){
+        // Remove all classes from the elements
+        $(this).removeClass(settings.className).removeClass(settings.focusName);
+        // Execute the disappear callback
         exitCallback(this);
       }
     });
 
   };
 
-}(jQuery));
+}(jQuery, window));
